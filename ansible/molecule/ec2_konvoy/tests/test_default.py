cd4f830e8532d4396bddb20119f2275c4b952684
Jan Ulferts <julferts@d2iq.com>
2021-07-01 17:14:00 +0200

    add base molecule testing

---
+++ ansible/molecule/ec2_konvoy/tests/test_default.py
@@ -0,0 +1,26 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_containerd_running_and_enabled(host):
+    containerd = host.service("containerd")
+    assert containerd.is_enabled
+
+
+def test_kubelet_running_and_enabled(host):
+    kubelet = host.service("kubelet")
+    assert kubelet.is_enabled
+
+
+def test_kubectl_avail(host):
+    cmd = host.run("type kubectl")
+    assert cmd.succeeded is True
+
+
+def test_kubeadm_avail(host):
+    cmd = host.run("type kubeadm")
+    assert cmd.succeeded is True
