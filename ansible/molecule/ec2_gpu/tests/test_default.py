71ad4be4606077ba087defbda8fceb83db4bec52
Jan Ulferts <julferts@d2iq.com>
2021-07-12 14:35:54 +0200

    fix format in tests

--- ansible/molecule/ec2_gpu/tests/test_default.py
+++ ansible/molecule/ec2_gpu/tests/test_default.py
@@ -3,12 +3,13 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
 def test_nvidia_smi(host):
     cmd = host.run("sudo nvidia-smi -L")
-    assert 'GPU 0' in cmd.stdout
+    assert "GPU 0" in cmd.stdout
 
 
 def test_cuda11_caps(host):
