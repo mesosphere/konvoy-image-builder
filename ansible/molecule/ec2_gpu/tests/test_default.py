a188603093ae2ae843edcf33b4dbf4a5b55ce74d
Till Toenshoff <toenshoff@me.com>
2021-11-26 20:33:07 -0500

    chore: GPU test update for flatcar

--- ansible/molecule/ec2_gpu/tests/test_default.py
+++ ansible/molecule/ec2_gpu/tests/test_default.py
@@ -8,13 +8,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_nvidia_smi(host):
-    cmd = host.run("sudo nvidia-smi -L")
+    cmd = host.run("bash -c 'PATH=$PATH:/opt/bin sudo nvidia-smi -L'")
     assert "GPU 0" in cmd.stdout
 
 
 def test_cuda11_caps(host):
     nvidia_caps = host.file("/dev/nvidia-caps")
-
     # only cuda 11 has those files.
     if nvidia_caps.exists:
         assert True is nvidia_caps.is_directory
@@ -22,15 +21,14 @@ def test_cuda11_caps(host):
 
 def test_cuda10_devices(host):
     nvidia_dev = host.file("/dev/nvidia0")
-
     assert True is nvidia_dev.exists
 
 
 def test_nvidia_container_cli_avail(host):
-    cmd = host.run("type nvidia-container-cli")
+    cmd = host.run("bash -c 'PATH=$PATH:/opt/bin type nvidia-container-cli'")
     assert cmd.succeeded is True
 
 
 def test_nvidia_container_runtime_avail(host):
-    cmd = host.run("type nvidia-container-runtime")
+    cmd = host.run("bash -c 'PATH=$PATH:/opt/bin type nvidia-container-runtime'")
     assert cmd.succeeded is True
