96264d5a120e70c7c4443811710cda80209da1b7
Joe Julian <me@joejulian.name>
2022-01-26 12:45:35 -0800

    fix: let the user know we are done

--- cmd/konvoy-image/main.go
+++ cmd/konvoy-image/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/mesosphere/konvoy-image-builder/cmd/konvoy-image/cmd"
+import (
+	"fmt"
+
+	"github.com/mesosphere/konvoy-image-builder/cmd/konvoy-image/cmd"
+)
 
 func main() {
 	cmd.Execute()
+	fmt.Println("Done!")
 }
