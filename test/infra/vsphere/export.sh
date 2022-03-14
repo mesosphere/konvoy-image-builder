b5febd32f78188ac333bb7967b9781691e286338
faiq <faiqrazarizvi@gmail.com>
2022-03-14 13:59:15 -0700

    fix: wire up packer extras correctly

--- test/infra/vsphere/export.sh
+++ test/infra/vsphere/export.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#CURDIR is populated from the make target
+echo "${SSH_BASTION_KEY_CONTENTS}" | sed 's/\\n/\n/g' >> vsphere-tests.pem
+chmod 600 vsphere-tests.pem
 envsubst < test/infra/vsphere/packer-vsphere-airgap.yaml.tmpl >> packer-vsphere-airgap.yaml
