587728166c3ff90646a00b2363de1a33d530e091
alejandroEsc <jaescobar.cell@gmail.com>
2022-05-18 10:53:59 -0400

    adding skeleton for gcp command to build images

---
+++ cmd/konvoy-image/cmd/gcp.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	flag "github.com/spf13/pflag"
+
+	"github.com/mesosphere/konvoy-image-builder/pkg/app"
+)
+
+var (
+	gcpExample = "gpc ... images/azure/centos-7.yaml"
+	gcpUse     = "gcp <image.yaml>"
+)
+
+var gcpBuildCmd = &cobra.Command{
+	Use:     gcpUse,
+	Short:   "build and provision gcp images",
+	Example: gcpExample,
+	Args:    cobra.ExactArgs(1),
+	Hidden:  true,
+	Run: func(cmd *cobra.Command, args []string) {
+		runBuild(args[0])
+	},
+}
+
+func initBuildGCP() {
+	fs := gcpBuildCmd.Flags()
+
+	initGenerateFlags(fs, &buildFlags.generateCLIFlags)
+	initGCPFlags(fs, &buildFlags.generateCLIFlags)
+
+	addBuildArgs(fs, &buildFlags)
+}
+
+func initGCPFlags(fs *flag.FlagSet, gFlags *generateCLIFlags) {
+	gFlags.userArgs.Azure = &app.AzureArgs{}
+	addGCPArgs(fs, gFlags.userArgs.Azure)
+}
+
+func addGCPArgs(fs *flag.FlagSet, azure *app.AzureArgs) {
+
+}
