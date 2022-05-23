fbed828fa54a590cf9efe2bebf257a553fb00a49
shalin patel <spatel@d2iq.com>
2022-05-23 16:41:13 -0700

    ci: add fips build chain targets

--- .teamcity/_self/KIBProject.kt
+++ .teamcity/_self/KIBProject.kt
@@ -1,15 +1,19 @@
 package _self
 
-import _self.buildTypes.*
+import _self.buildTypes.BuildChainAMI
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
-import jetbrains.buildServer.configs.kotlin.v2019_2.RelativeId
-import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.buildReportTab
 
 
 object KIBProject: Project({
-   val nonFIPSbuildTargets = listOf("centos7", "rhel79", "rhel82", "rhel84")
-   var nonFIPSUpstreamProjectId = "MesosphereOnly_ClosedSource_SecureSigning_BuildNokmemRpmRepos"
-   for (target in nonFIPSbuildTargets) {
-       buildType(BuildChainAMI(target, nonFIPSUpstreamProjectId))
+   val buildTargets = listOf("centos7", "rhel79", "rhel82", "rhel84")
+   val upstreamProjectId = "MesosphereOnly_ClosedSource_SecureSigning_BuildNokmemRpmRepos"
+   for (target in buildTargets) {
+       buildType(BuildChainAMI(target, upstreamProjectId))
+   }
+
+    val fipsBuildTargets = listOf("centos7-fips", "rhel84-fips")
+    val fipsUpstreamProjectId = "MesosphereOnly_ClosedSource_SecureSigning_BuildFipsRpmRepos"
+    for (target in fipsBuildTargets) {
+       buildType(BuildChainAMI(target, fipsUpstreamProjectId))
    }
 })
--- .teamcity/_self/buildTypes/BuildChainAMI.kt
+++ .teamcity/_self/buildTypes/BuildChainAMI.kt
@@ -1,5 +1,6 @@
 package _self.buildTypes
 
+import jetbrains.buildServer.configs.kotlin.v10.toExtId
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
@@ -7,57 +8,57 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.retryBuild
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 class BuildChainAMI(osTarget: String, upstreamDependency: String ) : BuildType({
-    id("BuildChainAMI${osTarget}")
-    name = "Build Chain - AMI ${osTarget}"
+    id("BuildChainAMI${osTarget}".toExtId())
+    name = "Build Chain - AMI $osTarget"
 
-    steps {
+     steps {
 
-        script {
-            name = "Build ${osTarget} AMI"
-            scriptContent = """
-                cat <<EOF > buildchain-overrides.yaml
-                build_name_extra: -buildchain
-                EOF
+         script {
+             name = "Build $osTarget AMI"
+             scriptContent = """
+                 cat <<EOF > buildchain-overrides.yaml
+                 build_name_extra: -buildchain
+                 EOF
                 
-                make devkit.run WHAT="make ${osTarget} ADDITIONAL_OVERRIDES=buildchain-overrides.yaml ADDITIONAL_ARGS=\"--extra-vars kubernetes-version=%dep.${upstreamDependency}.teamcity.build.branch%\" BUILD_DRY_RUN=true"
-            """.trimIndent()
-        }
+                 make devkit.run WHAT="make $osTarget ADDITIONAL_OVERRIDES=buildchain-overrides.yaml ADDITIONAL_ARGS=\"--extra-vars kubernetes-version=%dep.${upstreamDependency}.teamcity.build.branch%\" BUILD_DRY_RUN=true"
+             """.trimIndent()
+         }
 
-    }
-    vcs {
-        root(DslContext.settingsRoot)
-    }
+     }
+     vcs {
+         root(DslContext.settingsRoot)
+     }
 
-    triggers {
-        vcs {
-            id = "vcsTrigger"
-            enabled = false
-            branchFilter = """
-                +:*
-                -:<default>
-            """.trimIndent()
-        }
-        finishBuildTrigger {
-            id = "TRIGGER_153"
-            buildType = "${upstreamDependency}"
-            successfulOnly = true
-            branchFilter = """
-                +:v*
-                -:v1.21.0
-            """.trimIndent()
-        }
-        retryBuild {
-            id = "retryBuildTrigger"
-            delaySeconds = 120
-        }
-    }
+     triggers {
+         vcs {
+             id = "vcsTrigger"
+             enabled = false
+             branchFilter = """
+                 +:*
+                 -:<default>
+             """.trimIndent()
+         }
+         finishBuildTrigger {
+             id = "TRIGGER_153"
+             buildType = upstreamDependency
+             successfulOnly = true
+             branchFilter = """
+                 +:v*
+                 -:v1.21.0
+             """.trimIndent()
+         }
+         retryBuild {
+             id = "retryBuildTrigger"
+             delaySeconds = 120
+         }
+     }
 
-    dependencies {
-        snapshot(AbsoluteId("${upstreamDependency}")) {
-        }
-    }
+     dependencies {
+         snapshot(AbsoluteId(upstreamDependency)) {
+         }
+     }
 
-    requirements {
-        exists("DOCKER_VERSION", "RQ_26")
-    }
+     requirements {
+         exists("DOCKER_VERSION", "RQ_26")
+     }
 })
