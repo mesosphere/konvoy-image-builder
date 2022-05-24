393bc26599be97348cc2d7eeb70b03f6f2d84a73
shalin patel <spatel@d2iq.com>
2022-05-23 17:54:00 -0700

    ci: fix kotlin formatting

--- .teamcity/_self/KIBProject.kt
+++ .teamcity/_self/KIBProject.kt
@@ -3,17 +3,16 @@ package _self
 import _self.buildTypes.BuildChainAMI
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 
-
-object KIBProject: Project({
-   val buildTargets = listOf("centos7", "rhel79", "rhel82", "rhel84")
-   val upstreamProjectId = "MesosphereOnly_ClosedSource_SecureSigning_BuildNokmemRpmRepos"
-   for (target in buildTargets) {
-       buildType(BuildChainAMI(target, upstreamProjectId))
-   }
+object KIBProject : Project({
+    val buildTargets = listOf("centos7", "rhel79", "rhel82", "rhel84")
+    val upstreamProjectId = "MesosphereOnly_ClosedSource_SecureSigning_BuildNokmemRpmRepos"
+    for (target in buildTargets) {
+        buildType(BuildChainAMI(target, upstreamProjectId))
+    }
 
     val fipsBuildTargets = listOf("centos7-fips", "rhel84-fips")
     val fipsUpstreamProjectId = "MesosphereOnly_ClosedSource_SecureSigning_BuildFipsRpmRepos"
     for (target in fipsBuildTargets) {
-       buildType(BuildChainAMI(target, fipsUpstreamProjectId))
-   }
+        buildType(BuildChainAMI(target, fipsUpstreamProjectId))
+    }
 })
--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,3 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-
 /*
 The settings script is an entry point for defining a TeamCity
 project hierarchy. The script should contain a single call to the
