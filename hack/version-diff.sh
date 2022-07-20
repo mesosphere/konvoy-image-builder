15f46a8c129b2ed7e83482fcf94f796c7ec72808
faiq <faiqrazarizvi@gmail.com>
2022-07-20 10:40:22 -0700

    feat: add options to script for your own versions

--- hack/version-diff.sh
+++ hack/version-diff.sh
@@ -3,9 +3,25 @@
 # According to:
 # https://github.com/cli/cli/blob/235fdcdcc4c3df76adb4a84ecb0ba1ad470fbf5b/pkg/cmd/release/list/http.go#L32
 # the GH cli will list releases in descending order so if we list from newest to oldest
+while [ "$1" != "" ]; do
+  case $1 in
+  --version-latest)
+    shift
+    version_latest=$1
+    ;;
+  --version-previous)
+    shift
+    version_previous=$1
+    ;;
+    esac
+  shift
+done
 
-read -r vLatest vPrev <<< $(${GITHUB_CLI_BIN} release list -L 2 | awk '{print $1}' | xargs)
-DIFF=$(${SEMVER_CLI_BIN} diff $vLatest $vPrev)
+if [[ -z $version_latest  || -z $version_previous ]]; then
+  read -r version_latest version_previous <<< $(${GITHUB_CLI_BIN} release list -L 2 | awk '{print $1}' | xargs)
+fi
+
+DIFF=$(${SEMVER_CLI_BIN} diff $version_latest $version_previous)
 PR_TYPE=""
 case $DIFF in
   patch)
