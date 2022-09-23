082fb79b5e45aec1b9376fd92a1b3e0d888d72c8
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2022-09-23 07:51:25 -0700

    ci: use d2iq.com URL to download offline bundles

--- make/images.mk
+++ make/images.mk
@@ -12,7 +12,7 @@ COMMA := ,
 NULL :=
 SPACE := $(NULL) $(NULL)
 
-AIRGAPPED_BUNDLE_URL_PREFIX ?= downloads.mesosphere.io/dkp
+AIRGAPPED_BUNDLE_URL_PREFIX ?= downloads.d2iq.com/dkp
 ARTIFACTS_DIR ?= artifacts/
 CONTAINERD_URL ?= https://packages.d2iq.com/dkp/containerd
 NVIDIA_URL ?= https://download.nvidia.com/XFree86/Linux-x86_64
