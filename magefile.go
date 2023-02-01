580e9fa2065beeb4c49619256bcbf1932ad589fa
faiq <faiqrazarizvi@gmail.com>
2023-02-01 13:41:24 -0500

    fix: magefile logic

--- magefile.go
+++ magefile.go
@@ -230,8 +230,10 @@ func getBuildPath(buildOS, buildInfra string) string {
 	formattedOS = strings.Replace(formattedOS, ".", "", 1)
 
 	formattedOS = strings.Replace(formattedOS, "redhat", "rhel", 1)
-	formattedOS = strings.Replace(formattedOS, "1804", "18", 1)
-	formattedOS = strings.Replace(formattedOS, "2004", "20", 1)
+	if buildInfra == aws {
+		formattedOS = strings.Replace(formattedOS, "1804", "18", 1)
+		formattedOS = strings.Replace(formattedOS, "2004", "20", 1)
+	}
 
 	fileForOS := fmt.Sprintf("%s.yaml", formattedOS)
 
