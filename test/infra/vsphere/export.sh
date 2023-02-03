f1f469a8d5fb5f5fd2de1944aa1c7bc6a15c7f70
faiq <faiqrazarizvi@gmail.com>
2023-02-03 14:54:50 -0500

    ci: eval ssh agent in bash

--- test/infra/vsphere/export.sh
+++ test/infra/vsphere/export.sh
@@ -2,4 +2,6 @@
 # shellcheck disable=SC2001
 echo "${SSH_BASTION_KEY_CONTENTS}" | sed 's/\\n/\n/g' >> vsphere-tests.pem
 chmod 600 vsphere-tests.pem
+eval $(ssh-agent)
+ssh-add vsphere-tests.pem
 "${ENVSUBST_ASSETS}"/envsubst < test/infra/vsphere/packer-vsphere-airgap.yaml.tmpl >> "$1"
