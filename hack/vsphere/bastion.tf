19964ca0d2ab30020f7740007834772452e3611e
supershal <spatel@d2iq.com>
2023-07-05 17:15:43 -0700

    ci: refactor scripts to create bastion host

---
+++ hack/vsphere/bastion.tf
@@ -0,0 +1,25 @@
+module "bastion_node" {
+  source = "github.com/mesosphere/vcenter-tools/modules/vmclone"
+
+  node_name      = "konvoy-image-builder-bastion"
+  ssh_public_key =  file(var.ssh_public_key)
+
+  datastore_name       = var.datastore_name
+  datastore_is_cluster = false
+  vm_template_name = var.bastion_base_template
+  resource_pool_name = var.resource_pool_name
+  root_volume_size = var.root_volume_size #80
+  vsphere_folder = var.vsphere_folder
+  ssh_user = var.ssh_user
+  custom_attribute_owner      = var.bastion_owner
+  custom_attribute_expiration = "24h"
+  vsphere_network = var.vsphere_network
+}
+
+output "bastion_node_ssh_nat_address" {
+  value = "${module.bastion_node.nat_address}"
+}
+
+output "bastion_node_ssh_nat_port" {
+  value = "${module.bastion_node.nat_ssh_port}"
+}
---
+++ hack/vsphere/variables.tf
@@ -0,0 +1,45 @@
+variable "datastore_name" {
+  description = "The datastore name"
+}
+
+variable "bastion_base_template" {
+  description = "base template name"
+  default     = "os-qualification-templates/d2iq-base-RockyLinux-9.1"
+}
+
+variable "resource_pool_name" {
+  description = "The resource pool name"
+  default     = "cluster-api"
+}
+
+variable "root_volume_size" {
+  description = "Disk size for root volume"
+  default     = 80
+}
+
+variable "vsphere_folder" {
+  description = "folder name to store the VM"
+  default     = "cluster-api"
+}
+
+variable "ssh_user" {
+  description = "The root user"
+  default     = "centos"
+}
+
+
+variable "bastion_owner" {
+  description = "The root user"
+  default     = "Konvoy image builder"
+}
+
+variable "ssh_public_key" {
+  description = "Path to the SSH Public key. for example: ~/.ssh/id_rsa.pub"
+  type        = string
+}
+
+variable "vsphere_network" {
+  description = "vsphere network"
+  type        = string
+  default = "Airgapped"
+}
