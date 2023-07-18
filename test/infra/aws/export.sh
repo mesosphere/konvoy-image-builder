b0e31901b2d15907640301e645cbeacb39877dde
Shalin Patel <spatel@d2iq.com>
2023-07-18 14:28:21 -0700

    ci: upload KIB release template to cloud director organization  (#855)
    * ci: terraform script to upload KIB vsphere template to VCD
    
    * ci: github actions workflow to upload template to vcd org
    
    * ci: fix path to the terraform state
    
    * ci: upload template to VCD when vsphere release is finished

--- test/infra/aws/export.sh
+++ test/infra/aws/export.sh
@@ -4,9 +4,9 @@ set -o pipefail
 set -o nounset
 
 # shellcheck disable=SC2155
-export AWS_VPC_ID="$("${TERRAFORM_BIN}" -chdir=.local/infra/aws output -raw vpc_id)"
+export AWS_VPC_ID="$("${TERRAFORM_BIN}" -chdir="${INFRA_MODULES_DIR}/aws"  output -raw vpc_id)"
 # shellcheck disable=SC2155
-export AWS_SECURITY_GROUP_ID="$("${TERRAFORM_BIN}" -chdir=.local/infra/aws output -raw security_group_id)"
+export AWS_SECURITY_GROUP_ID="$("${TERRAFORM_BIN}" -chdir="${INFRA_MODULES_DIR}/aws"  output -raw security_group_id)"
 # shellcheck disable=SC2155
-export AWS_SUBNET_ID="$("${TERRAFORM_BIN}" -chdir=.local/infra/aws output -raw public_subnets)"
-"${ENVSUBST_ASSETS}"/envsubst < .local/infra/aws/packer-aws-offline-override.yaml.tmpl >> "$1"
+export AWS_SUBNET_ID="$("${TERRAFORM_BIN}" -chdir="${INFRA_MODULES_DIR}/aws" output -raw public_subnets)"
+"${ENVSUBST_ASSETS}"/envsubst < "${INFRA_MODULES_DIR}"/aws/packer-aws-offline-override.yaml.tmpl >> "$1"
